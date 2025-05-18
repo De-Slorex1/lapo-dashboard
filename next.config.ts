f6e70289c36e7729d7f45544923d9af598990716
De-Slorex1 <jacobsamson2000@gmail.com>
2025-05-18 23:30:13 +0100

    great

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/old-path',
+        destination: '/new-path',
+      },
+    ];
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
